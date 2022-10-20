84f16414722135421de05acb68a435d4d0e1d843
Stephan Griesel <sgriesel@gmail.com>
2022-10-20 16:37:07 +0200

    add icons

---
+++ src/components/icons/Check.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Check = () => {
+  return (
+    <div><svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" fill="#000000" viewBox="0 0 256 256"><rect width="256" height="256" fill="none"></rect><polyline points="216 72 104 184 48 128" fill="none" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-width="16"></polyline></svg></div>
+  )
+}
+
+export default Check
