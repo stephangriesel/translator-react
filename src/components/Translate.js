fd481d34636277155fc4f8412c85df23fe66ab78
Stephan Griesel <sgriesel@gmail.com>
2022-10-20 17:47:06 +0200

    get data

--- src/components/Translate.js
+++ src/components/Translate.js
@@ -1,33 +1,41 @@
 import React, { useState, useEffect } from 'react';
 import TranslateIcon from './icons/TranslateIcon';
+import axios from 'axios';
 
 const Translate = () => {
     const [inputText, setInputText] = useState('');
-    const [resultText, setResultText] = useState('')
+    const [resultText, setResultText] = useState('');
+    const [languagesList, setLanguagesList] = useState([])
 
     const translateText = () => {
         setResultText(inputText);
     }
+    
+    useEffect(() => {
+        axios.get('https://libretranslate.com/languages')
+        .then((response) => {
+            setLanguagesList(response.data);
+        })
+
+    },[])
     return (
         <div className='h-screen flex flex-col justify-center w-full items-center'>
             <h2>Translator</h2>
             <div className='flex flex-col'>
                 <textarea
-                    className='p-4 m-2 bg-neutral-400'
+                    className='p-4 m-2 bg-neutral-400 h-40'
                     onChange={(e) => setInputText(e.target.value)}
                 />
-                <select className='m-2 p-2' name="cars" id="cars" form="carform">
-                    <option value="za">Afrikaans</option>
-                    <option value="gb">English</option>
-                    <option value="nl">Dutch</option>
-                    <option value="ger">German</option>
-                    <option value="fr">French</option>
-                    <option value="it">Italian</option>
-                    <option value="es">Spanish</option>
+                <select className='m-2 p-2'>
+                    {languagesList.map((language) => {
+                        return (
+                            <option value={language.code}>{language.name}</option>
+                        )
+                    })}
                 </select>
-                <textarea 
-                className='p-4 m-2 bg-neutral-400' 
-                value={resultText}
+                <textarea
+                    className='p-4 m-2 bg-neutral-400 h-40'
+                    value={resultText}
                 />
                 <button
                     className='m-2 p-2 flex justify-center'
