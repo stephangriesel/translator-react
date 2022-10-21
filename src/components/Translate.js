350a2c906d4612af6e6257da2fa286ff8acfd824
Stephan Griesel <sgriesel@gmail.com>
2022-10-21 15:20:07 +0200

    fix errors

--- src/components/Translate.js
+++ src/components/Translate.js
@@ -1,90 +1,88 @@
-import React, { useState, useEffect } from 'react';
-import TranslateIcon from './icons/TranslateIcon';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import TranslateIcon from "./icons/TranslateIcon";
+import axios from "axios";
 
 const Translate = () => {
-    const [inputText, setInputText] = useState('');
-    const [resultText, setResultText] = useState('');
-    const [selectedLanguageKey, setSelectedLanguageKey] = useState('')
-    const [languagesList, setLanguagesList] = useState([]);
-    const [detectLanguageKey, setDetectLanguageKey] = useState('')
+  const [inputText, setInputText] = useState("");
+  const [resultText, setResultText] = useState("");
+  const [selectedLanguageKey, setSelectedLanguageKey] = useState("");
+  const [languagesList, setLanguagesList] = useState([]);
+  const [detectLanguageKey, setDetectLanguageKey] = useState("");
 
-    const getLanguageSource = () => {
-        axios.post(`https://libretranslate.com/detect`, {
-            q: inputText,
-            api_key: '3857b65a-f5e0-42a9-a135-5dcb491681c4',
-            source: '',
-        })
-        .then((response) => {
-            setDetectLanguageKey(response.data[0].language);
-        })
-    }
-    const translateText = () => {
-        setResultText(inputText);
+  const getLanguageSource = () => {
+    axios
+      .post(`https://libretranslate.com/detect`, {
+        q: inputText,
+        api_key: "3857b65a-f5e0-42a9-a135-5dcb491681c4",
+        source: "",
+      })
+      .then(response => {
+        setDetectLanguageKey(response.data[0].language);
+      });
+  };
+  const translateText = () => {
+    setResultText(inputText);
 
-        getLanguageSource();
+    getLanguageSource();
 
-        let data = {
-            q: inputText,
-            source: detectLanguageKey,
-            target: selectedLanguageKey,
-            api_key: '3857b65a-f5e0-42a9-a135-5dcb491681c4'
-        }
-        axios.post(`https://libretranslate.com/translate`, data)
-        .then((response) => {
-            setResultText(response.data.translatedText);
-        })
-    }
+    let data = {
+      q: inputText,
+      source: detectLanguageKey,
+      target: selectedLanguageKey,
+      api_key: "3857b65a-f5e0-42a9-a135-5dcb491681c4",
+    };
+    axios.post(`https://libretranslate.com/translate`, data).then(response => {
+      setResultText(response.data.translatedText);
+    });
+  };
 
-    const languageKey = (selectedLanguage) => {
-        setSelectedLanguageKey(selectedLanguage.target.value)
-    }
+  const languageKey = selectedLanguage => {
+    setSelectedLanguageKey(selectedLanguage.target.value);
+  };
 
-    useEffect(() => {
-        let data = {
-            q: inputText,
-            source: detectLanguageKey,
-            target: selectedLanguageKey,
-            api_key: '3857b65a-f5e0-42a9-a135-5dcb491681c4'
-        }
+  useEffect(() => {
+    let data = {
+      q: inputText,
+      source: detectLanguageKey,
+      target: selectedLanguageKey,
+      api_key: "3857b65a-f5e0-42a9-a135-5dcb491681c4",
+    };
 
-        axios.get('https://libretranslate.com/languages', data)
-            .then((response) => {
-                setLanguagesList(response.data);
-            })
+    axios.get("https://libretranslate.com/languages", data).then(response => {
+      setLanguagesList(response.data);
+    });
 
-            getLanguageSource();
-    }, [inputText])
-    return (
-        <div className='h-screen flex flex-col justify-center w-full items-center'>
-            <h2>Translator</h2>
-            <div className='flex flex-col'>
-                <textarea
-                    className='p-4 m-2 bg-neutral-400 h-40'
-                    onChange={(e) => setInputText(e.target.value)}
-                />
-                <select className='m-2 p-2' onChange={languageKey}>
-                    <option>Select language</option>
-                    {languagesList.map((language) => {
-                        return (
-                            <option key={language.code} value={language.code}>{language.name}</option>
-                        )
-                    })}
-                </select>
-                <textarea
-                    className='p-4 m-2 bg-neutral-400 h-40'
-                    value={resultText}
-                />
-                <button
-                    className='m-2 p-2 flex justify-center'
-                    onClick={translateText}
-                >
-                    <TranslateIcon />
-                </button>
-            </div>
-        </div>
+    getLanguageSource();
+  }, [inputText]);
+  return (
+    <div className="h-screen flex flex-col justify-center w-full items-center">
+      <h2>Translator</h2>
+      <div className="flex flex-col">
+        <textarea
+          className="p-4 m-2 bg-neutral-400 h-40"
+          onChange={e => setInputText(e.target.value)}
+        />
+        <select className="m-2 p-2" onChange={languageKey}>
+          <option>Select language</option>
+          {languagesList.map(language => {
+            return (
+              <option key={language.code} readOnly value={language.code}>
+                {language.name}
+              </option>
+            );
+          })}
+        </select>
+        <textarea
+          className="p-4 m-2 bg-neutral-400 h-40"
+          readOnly
+          value={resultText}
+        />
+        <button className="m-2 p-2 flex justify-center" onClick={translateText}>
+          <TranslateIcon />
+        </button>
+      </div>
+    </div>
+  );
+};
 
-    )
-}
-
-export default Translate
+export default Translate;
--- src/components/icons/Arrows.js
+++ src/components/icons/Arrows.js
@@ -1,11 +1,57 @@
-import React from 'react'
+import React from "react";
 
 const Arrows = () => {
   return (
     <div>
-      <svg xmlns="http://www.w3.org/2000/svg" width="192" height="192" fill="#000000" viewBox="0 0 256 256"><rect width="256" height="256" fill="none"></rect><polyline points="176 144 208 176 176 208" fill="none" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-width="16"></polyline><line x1="48" y1="176" x2="208" y2="176" fill="none" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-width="16"></line><polyline points="80 112 48 80 80 48" fill="none" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-width="16"></polyline><line x1="208" y1="80" x2="48" y2="80" fill="none" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-width="16"></line></svg>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="192"
+        height="192"
+        fill="#000000"
+        viewBox="0 0 256 256"
+      >
+        <rect width="256" height="256" fill="none"></rect>
+        <polyline
+          points="176 144 208 176 176 208"
+          fill="none"
+          stroke="#000000"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="16"
+        ></polyline>
+        <line
+          x1="48"
+          y1="176"
+          x2="208"
+          y2="176"
+          fill="none"
+          stroke="#000000"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="16"
+        ></line>
+        <polyline
+          points="80 112 48 80 80 48"
+          fill="none"
+          stroke="#000000"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="16"
+        ></polyline>
+        <line
+          x1="208"
+          y1="80"
+          x2="48"
+          y2="80"
+          fill="none"
+          stroke="#000000"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="16"
+        ></line>
+      </svg>
     </div>
-  )
-}
+  );
+};
 
-export default Arrows
+export default Arrows;
